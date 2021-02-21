a2464aad5404aafa8b12452d8b0cbef5862507a8
klpbbs <62373803+klpbbs@users.noreply.github.com>
2021-02-21 21:25:52 +0800

    Update pt.php

--- pt.php
+++ pt.php
@@ -8,6 +8,6 @@ $url = $_GET["url"];
 const dp = new DPlayer({
 container: document.getElementById('dplayer'),
 volume: 1,
-video: {url: '<?php echo($url) ?>'},
-contextmenu: [{text: '苦力怕论坛',link: 'https://klpbbs.com'},],});
+video: {url: '<?php echo($url) ?>'}, 
+contextmenu: [{text: '苦力怕论坛',link: 'https://klpbbs.com'},],}); //右键菜单自定义添加项，其他需要到res中修改
 </script>
