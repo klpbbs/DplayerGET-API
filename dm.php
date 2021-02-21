1f075d20059971aee3745b44a9c1519f3aca306d
klpbbs <62373803+klpbbs@users.noreply.github.com>
2021-02-21 21:25:29 +0800

    Update dm.php

--- dm.php
+++ dm.php
@@ -10,6 +10,6 @@ const dp = new DPlayer({
 container: document.getElementById('dplayer'),
 volume: 1,
 video: {url: '<?php echo($url) ?>'},
-danmaku: {id:'<?php echo($dm) ?>',api: 'https://dplayer.alone88.cn/',unlimited: true,},
-contextmenu: [{text: '苦力怕论坛',link: 'https://klpbbs.com'},],});
+danmaku: {id:'<?php echo($dm) ?>',api: 'https://dplayer.alone88.cn/',unlimited: true,}, //弹幕库，网上随便找了一个，最好用uuid作为id
+contextmenu: [{text: '苦力怕论坛',link: 'https://klpbbs.com'},],});  //右键菜单自定义添加项，其他需要到res中修改
 </script>
